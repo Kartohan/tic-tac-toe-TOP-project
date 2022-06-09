89ab661f15c147bfcc5c6ca55166d29721ad9e06
Kartohan <Kartohan@gmail.com>
2022-06-09 20:20:36 +0300

    Add 1.0 ver of game

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # tic-tac-toe-TOP-project
+This project really turned my brain inside out. I've learned a bit about modular JS, but I'll try my best! Understand the logic of the game. AI I borrowed in this article, thanks to the author. I tried very hard to understand the code, but it turned out so-so. I still have a lot to learn!
+There are two game modes, two players, and player versus computer.
+You can also play on your phone.
+<br>
+
+09.06.22 - Two players mode. AI vs Player mode. Phone support. (1.0 ver)<br>
+
+Unfortunately (or fortunately) I took the invincible AI algorithm. I wonder if anyone can win :)<br>
+
+[AI Minimax Article](https://www.freecodecamp.org/news/how-to-make-your-tic-tac-toe-game-unbeatable-by-using-the-minimax-algorithm-9d690bad4b37/)
+[Background gradient](https://www.gradientmagic.com/collection/standard/gradient/1563407267183)
--- app.js
+++ app.js
@@ -3,7 +3,8 @@
     let objects = (() => {
         let gameboardArr = ['', '', '', '', '', '', '', '', ''];
         let form = document.querySelector('form');
-        let selection = document.querySelector('select');
+        let selection = document.querySelector('#side');
+        let selectionOpp = document.querySelector('#opp');
         let settings = document.querySelector('.game-settings');
         let [...gameSeq] = document.querySelectorAll('.game-sequence');
         let gameContainer = document.querySelector('.game-container');
@@ -11,16 +12,23 @@
         let retryBtn = document.querySelector('.retry');
         let playerOne;
         let playerTwo;
-        let winCombinations = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]  
+        let winner = (board, player) => {
+            if (
+            (board[0] == player && board[1] == player && board[2] == player) ||
+            (board[3] == player && board[4] == player && board[5] == player) ||
+            (board[6] == player && board[7] == player && board[8] == player) ||
+            (board[0] == player && board[3] == player && board[6] == player) ||
+            (board[1] == player && board[4] == player && board[7] == player) ||
+            (board[2] == player && board[5] == player && board[8] == player) ||
+            (board[0] == player && board[4] == player && board[8] == player) ||
+            (board[2] == player && board[4] == player && board[6] == player)
+            ) {
+            return true;
+            } else {
+            return false;
+            }
+           }
+
         let gameboardChange = (value, index) => {
             if (gameboardArr[index] === value) {
                 return;
@@ -38,21 +46,22 @@
             playerOne,
             playerTwo,
             gameSeq,
-            winCombinations,
             winHeader,
             retryBtn,
+            selectionOpp,
             gameboardChange,
             gameboardReadIndex,
+            winner
         }
     })();
 
     let Player = (side) => {
         let isPlaying = false;
+        let isComputer = false;
         let getSide = () => side;
-        let isWinner = false;
         return {
             isPlaying,
-            isWinner,
+            isComputer,
             getSide
         }
     }
@@ -72,7 +81,12 @@
                     objects.playerOne.isPlaying = false;
                     objects.playerTwo.isPlaying = true;
                     checkWin();
-                } else if (objects.playerTwo.isPlaying === true) {
+                    if (objects.playerTwo.isComputer === true) {
+                        objects.gameboardChange(objects.playerTwo.getSide(), minimax(gameBoard(gameArr()), objects.playerTwo.getSide()).index);
+                        objects.playerOne.isPlaying = true;
+                        checkWin();
+                    }
+                } else if (objects.playerTwo.isPlaying === true && objects.playerTwo.isComputer === false) {
                     objects.gameboardChange(objects.playerTwo.getSide(), e.target.dataset.id);
                     objects.playerOne.isPlaying = true;
                     objects.playerTwo.isPlaying = false;
@@ -81,14 +95,13 @@
             }
         }
         let winDisplay = (side) => {
-            objects.gameContainer.style.display = 'none';
             objects.winHeader.style.display = 'block';
             objects.winHeader.textContent = side + ' \'s wins';
             objects.retryBtn.style.display = 'block';
         }
         let checkWin = () => {
-            let winX = objects.winCombinations.some((combination) => combination.every((i) => objects.gameboardReadIndex(i).includes(objects.playerOne.getSide())));
-            let winO = objects.winCombinations.some((combination) => combination.every((i) => objects.gameboardReadIndex(i).includes(objects.playerTwo.getSide())));
+            let winOne = objects.winner(gameArr(), objects.playerOne.getSide());
+            let winTwo = objects.winner(gameArr(), objects.playerTwo.getSide());
             let draw = () => {
                 let drawArr = [];
                 for (i = 0; i < 9; i++) {
@@ -100,10 +113,14 @@
                     objects.winHeader.textContent = 'Draw!';
                 }
             }
-            if (winX === true) {
+            if (winOne === true) {
                 winDisplay(objects.playerOne.getSide())
-            } else if (winO === true) {
+                objects.playerOne.isPlaying = false;
+                objects.playerTwo.isPlaying = false;
+            } else if (winTwo === true) {
                 winDisplay(objects.playerTwo.getSide())
+                objects.playerOne.isPlaying = false;
+                objects.playerTwo.isPlaying = false;
             } else {
                 draw();
             }
@@ -111,32 +128,109 @@
         let retryGame = () => {
             objects.retryBtn.style.display = 'none';
             objects.winHeader.style.display = 'none';
-            objects.settings.style.display = 'block';
-            objects.gameContainer.style.display = 'none';
+            objects.settings.style.display = 'none';
+            objects.gameContainer.style.display = 'grid';
+            objects.playerOne.isPlaying = true;
             for (let i = 0; i < 9; i++) {
                 objects.gameboardChange('', i);
             }
         }
+        let emptyIndexies = (board) => {
+            return board.filter(s => s != "O" && s != "X");
+        }
+        let gameArr = () => {
+            let arr = [];
+                for (i = 0; i < 9; i++) {
+                    arr.push(objects.gameboardReadIndex(i));
+                }
+            return arr;
+        }
+        let gameBoard = (board) => {
+            let arr = [];
+                for (i = 0; i < 9; i++) {
+                    if (board[i] === 'X' || board[i] === 'O') {
+                        arr.push(board[i]);
+                    } else if (board[i] === '') {
+                        arr.push(i);
+                    }
+                }
+                return arr;
+        }
+        let minimax = (newBoard, player) => {
+            let avaliSpots = emptyIndexies(newBoard);
+            if (objects.winner(newBoard, objects.playerOne.getSide())) {
+                return {score:-10}
+            } else if (objects.winner(newBoard, objects.playerTwo.getSide())) {
+                return {score:10}
+            } else if (avaliSpots.length === 0) {
+                return {score:0};
+            }
+            let moves = [];
+
+            for (let i = 0; i < avaliSpots.length; i++) {
+                let move = {};
+                move.index = newBoard[avaliSpots[i]];
+                newBoard[avaliSpots[i]] = player;
+
+                if (player == objects.playerTwo.getSide()){
+                var result = minimax(newBoard, objects.playerOne.getSide());
+                move.score = result.score;
+                }
+                else{
+                var result = minimax(newBoard, objects.playerTwo.getSide());
+                move.score = result.score;
+                }
+                newBoard[avaliSpots[i]] = move.index;
+                
+                moves.push(move);
+            }
+            let bestMove;
+            if(player === objects.playerTwo.getSide()){
+                var bestScore = -10000;
+                for(var i = 0; i < moves.length; i++){
+                if(moves[i].score > bestScore){
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+                }
+            } else {
+                var bestScore = 10000;
+                for(var i = 0; i < moves.length; i++){
+                if(moves[i].score < bestScore){
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+                }
+            }
+            return moves[bestMove];
+            }
         objects.gameContainer.addEventListener('click', addMark);
         objects.retryBtn.addEventListener('click', retryGame)
 
         return {
-            render
+            render,
+            minimax,
+            emptyIndexies,
+            gameBoard,
+            gameArr
         }
     })();
 
     let playerSelection = (() => {
         addPlayer = (e) => {
             e.preventDefault();
-            if (objects.selection.value === 'X') {
-                objects.playerOne = Player('X');
-                objects.playerTwo = Player('O');
-                objects.playerOne.isPlaying = true;
-            } else if (objects.selection.value === 'O') {
-                objects.playerOne = Player('O');
-                objects.playerTwo = Player('X');
-                objects.playerOne.isPlaying = true;
-            }
+                if (objects.selection.value === 'X') {
+                    objects.playerOne = Player('X');
+                    objects.playerTwo = Player('O');
+                    objects.playerOne.isPlaying = true;
+                } else if (objects.selection.value === 'O') {
+                    objects.playerOne = Player('O');
+                    objects.playerTwo = Player('X');
+                    objects.playerOne.isPlaying = true;
+                }
+                if (objects.selectionOpp.value === 'computer') {
+                    objects.playerTwo.isComputer = true;
+                }
             objects.gameContainer.style.display = 'grid';
             objects.settings.style.display = 'none';
         }
